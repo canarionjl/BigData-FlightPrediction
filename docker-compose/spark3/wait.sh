70dd3a2d49b631141575d33eeafb5b389c42ac96
canarionjl <canarionjl@gmail.com>
2024-01-13 00:09:04 +0100

    Docker compose with distributed spark processing is already working

--- docker-compose/spark3/wait.sh
+++ docker-compose/spark3/wait.sh
@@ -6,4 +6,4 @@ sleep 180
 
 echo "Cassandra should be ready"
 
-/opt/bitnami/spark/bin/spark-submit --class es.upm.dit.ging.predictor.MakePrediction --master local --packages com.datastax.spark:spark-cassandra-connector_2.12:3.4.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 /scala-2.12/flight_prediction_2.12-0.1.jar
+/spark/bin/spark-submit --class es.upm.dit.ging.predictor.MakePrediction --master spark://spark-master:7077 --packages com.datastax.spark:spark-cassandra-connector_2.12:3.4.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 /scala-2.12/flight_prediction_2.12-0.1.jar
